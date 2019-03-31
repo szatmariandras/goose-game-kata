382d437a9870afd4875dd62a7a9aea9a81719370
Andras Szatmari <strati@strati.hu>
2019-03-31 20:46:53 +0200

    Finishing touches on CommandParserTest

--- src/test/scala/hu/andrasszatmari/goosegame/CommandParserTest.scala
+++ src/test/scala/hu/andrasszatmari/goosegame/CommandParserTest.scala
@@ -7,25 +7,29 @@ class CommandParserTest extends FlatSpec with Matchers {
   behavior of "CommandParser.parse"
 
   it should "parse add player commands correctly" in {
-    CommandParser.parse("add player Béla") shouldBe Some(AddPlayer("Béla"))
-    CommandParser.parse("add player Béla_Kettő") shouldBe Some(AddPlayer("Béla_Kettő"))
-    CommandParser.parse("add player Béla Kettő") shouldBe None
+    CommandParser.parse("add player Pippo") shouldBe Some(AddPlayer("Pippo"))
+    CommandParser.parse("add player Pippo_Pippo") shouldBe Some(AddPlayer("Pippo_Pippo"))
+    CommandParser.parse("add player Pippo With Spaces") shouldBe None
   }
 
   it should "parse move player random commands correctly" in {
-    CommandParser.parse("move Béla") shouldBe Some(MovePlayerRandom("Béla"))
-    CommandParser.parse("move Béla_Kettő") shouldBe Some(MovePlayerRandom("Béla_Kettő"))
-    CommandParser.parse("move Béla Kettő") shouldBe None
+    CommandParser.parse("move Pippo") shouldBe Some(MovePlayerRandom("Pippo"))
+    CommandParser.parse("move Pippo_Pippo") shouldBe Some(MovePlayerRandom("Pippo_Pippo"))
+    CommandParser.parse("move Pippo With Spaces") shouldBe None
   }
 
   it should "parse move player commands correctly" in {
-    CommandParser.parse("move Béla 1, 2") shouldBe Some(MovePlayer("Béla", 1, 2))
-    CommandParser.parse("move Béla_Kettő 3, 4") shouldBe Some(MovePlayer("Béla_Kettő", 3, 4))
-    CommandParser.parse("move Béla Kettő 3, 4") shouldBe None
+    CommandParser.parse("move Pippo 1, 2") shouldBe Some(MovePlayer("Pippo", 1, 2))
+    CommandParser.parse("move Pippo_Pippo 3, 4") shouldBe Some(MovePlayer("Pippo_Pippo", 3, 4))
+    CommandParser.parse("move Pippo With Spaces 3, 4") shouldBe None
   }
 
   it should "parse exit command correctly" in {
     CommandParser.parse("exit") shouldBe Some(Exit)
+  }
+
+  it should "parse help command correctly" in {
+    CommandParser.parse("help") shouldBe Some(Help)
   }
 
 }
